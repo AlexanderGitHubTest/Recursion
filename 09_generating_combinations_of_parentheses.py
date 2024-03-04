d633a12d9d20a80c2aa67a6ba84fc80721489dcf
AlexanderGitHubTest <94744815+AlexanderGitHubTest@users.noreply.github.com>
2024-03-04 16:13:01 +0300

    Change: One recursive call was made instead of two

--- 09_generating_combinations_of_parentheses.py
+++ 09_generating_combinations_of_parentheses.py
@@ -2,6 +2,7 @@
 Generating all correct balanced combinations of parentheses.
 """
 
+from collections import namedtuple
 from typing import List
 
 def generating_combinations_of_parentheses(number_of_opening_parentheses: int) -> List[str]:
@@ -24,16 +25,30 @@ def generating_combinations_recursion(left_parentheses_count: int,
         and right_parentheses_count == 0):
         result_list.append(current_combination)
         return result_list   
-    if left_parentheses_count > 0:
-        generating_combinations_recursion(left_parentheses_count - 1,
-                                          right_parentheses_count,
-                                          current_combination + "(",
-                                          result_list
-                                         )
+    stack = []
+    Data_For_Next_Recursion = namedtuple('Data_For_Next_Recursion',
+                                         ['left_parentheses_count_new',
+                                          'right_parentheses_count_new',
+                                          'combination_new'])
     if right_parentheses_count > left_parentheses_count:
-        generating_combinations_recursion(left_parentheses_count,
-                                          right_parentheses_count - 1,
-                                          current_combination + ")",
+        stack.append(Data_For_Next_Recursion
+                     (left_parentheses_count,
+                      right_parentheses_count - 1,
+                      current_combination + ")"
+                     )
+                    )
+    if left_parentheses_count > 0:
+        stack.append(Data_For_Next_Recursion
+                     (left_parentheses_count - 1,
+                      right_parentheses_count,
+                      current_combination + "("
+                     )
+                    )
+    while len(stack) > 0:
+        data_for_next_recursion = stack.pop()
+        generating_combinations_recursion(data_for_next_recursion.left_parentheses_count_new,
+                                          data_for_next_recursion.right_parentheses_count_new,
+                                          data_for_next_recursion.combination_new,
                                           result_list
                                          )
     return result_list
